eace98ff49b00040b8b21d7c9fb2285b9018b5ae
Federico <fheisig@gmail.com>
2017-07-21 18:44:23 -0400

    bootstrap and static pages (sessions)

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,74 @@
+class SessionsController < ApplicationController
+
+	# Página Principal
+	def welcome
+
+	end
+
+	# Cambiar de carrera (Gorila)
+	def mi_carrera
+
+	end		
+
+	# Cambiar mis ramos (Gorila)
+	def mis_ramos
+
+	end
+
+	# mi_horario (Gorila) [Podriamos reemplazar esto por una lista]
+	def mi_horario
+
+	end
+
+	#Cambiar mis ramos
+	def mis_ramos
+
+	end
+
+	#Cambiar mis horarios
+	def mi_horario
+
+	end	
+
+	def mis_solicitudes
+		
+	end
+
+	def ensenar
+		
+	end
+
+	def busqueda_catedras
+		
+	end
+
+	def oauth_connect
+		
+	end
+
+	def facebook_login
+		
+	end	
+
+	# Login
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def destroy
+		
+	end
+
+	def counter
+		
+	end
+
+	def about_us
+		
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # You can have the root of your site routed with "root"
+  root 'sessions#welcome'
+
+  get 'quienes-somos' => 'sessions#about_us', as: :about_us
+
 end
