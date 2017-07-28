e536c0d147a5670794f83c04a63a205698289614
Federico <fheisig@gmail.com>
2017-07-28 16:07:47 -0400

    Add password reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727223506) do
+ActiveRecord::Schema.define(version: 20170728161231) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 20170727223506) do
     t.datetime "updated_at", null: false
     t.string "activation_digest"
     t.boolean "activated", default: false
+    t.string "reset_digest"
+    t.datetime "reset_sent_at"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_digest"], name: "index_users_on_remember_digest"
   end
